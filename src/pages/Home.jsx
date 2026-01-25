ac400622976916cce798c8889f306598821a58d5
parthkk <parth122004@gmail.com>
2026-01-25 11:28:59 +0530

    Added gradient blinds animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import GradientBlinds from '../components/GradientBlinds';
 
 export default function Home() {
   useEffect(() => {
@@ -24,11 +25,22 @@ export default function Home() {
 
   return (
     <>
-      {/* Animated Background - Unicorn Studio */}
-      <div className="aura-background-component fixed top-0 w-full h-screen -z-10" data-alpha-mask="80" style={{ maskImage: 'linear-gradient(to bottom, transparent, black 0%, black 80%, transparent)', WebkitMaskImage: 'linear-gradient(to bottom, transparent, black 0%, black 80%, transparent)', pointerEvents: 'none' }}>
-        <div className="aura-background-component top-0 w-full -z-10 absolute h-full">
-          <div data-us-project="FixNvEwvWwbu3QX9qC3F" className="absolute w-full h-full left-0 top-0 -z-10"></div>
-        </div>
+      {/* Animated Background - GradientBlinds */}
+      <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100vh', zIndex: 0, pointerEvents: 'none' }}>
+        <GradientBlinds
+          gradientColors={['#0ea5e9', '#3b82f6', '#8b5cf6']}
+          angle={0}
+          noise={0.3}
+          blindCount={12}
+          blindMinWidth={50}
+          spotlightRadius={0.5}
+          spotlightSoftness={1}
+          spotlightOpacity={1}
+          mouseDampening={0.15}
+          distortAmount={0}
+          shineDirection="left"
+          mixBlendMode="lighten"
+        />
       </div>
 
       {/* Hero Content */}
